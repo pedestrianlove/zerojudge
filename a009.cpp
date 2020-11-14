d72557f01b02a34a254a849a402981c94ee758f8
JHIH-SIOU LI <j.s.li@gapp.nthu.edu.tw>
2020-11-14 22:32:10 +0800

    update

---
+++ a009.cpp
@@ -0,0 +1,16 @@
+// Zerojudge a009.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	char tmp;
+	while (cin >> tmp) {
+		cout << (char) (tmp + 7);
+	}
+
+
+
+}
---
+++ a010.cpp
@@ -0,0 +1,19 @@
+// Zerojudge a010.cpp
+
+# include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int num;
+	while (cin >> num) {
+		
+	
+	
+	
+	}
+
+
+
+}
